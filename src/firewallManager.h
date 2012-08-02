0a02562a5ed80fed1870e7fcd01ef77567436bfc
Kehnin Dyer <Kehnin.Dyer@pnnl.gov>
2012-08-01 18:04:58 -0700

    finished factoring start element of parser

--- src/firewallManager.h
+++ src/firewallManager.h
@@ -99,15 +99,11 @@ using std::string;
         PROTOCOL_ERROR_OPEN_ERROR,
         PROTOCOL_ERROR_PARSE_ERROR,
         PROTOCOL_ERROR_ENTRY_NAME_ATTR_NOT_FOUND,
-        PROTOCOL_ERROR_TCP_SOURCE_UNKNOWN,
-        PROTOCOL_ERROR_TCP_DEST_UNKNOWN,
-        PROTOCOL_ERROR_UDP_SOURCE_UNKNOWN,
-        PROTOCOL_ERROR_UDP_DEST_UNKNOWN,
-        PROTOCOL_ERROR_ICMP_SOURCE_UNKNOWN,
-        PROTOCOL_ERROR_ICMP_DEST_UNKNOWN,
+        PROTOCOL_ERROR_TCP_SRCDEST_UNKNOWN,
+        PROTOCOL_ERROR_UDP_SRCDEST_UNKNOWN,
+        PROTOCOL_ERROR_ICMP_SRCDEST_UNKNOWN,
         PROTOCOL_ERROR_IP_PROTOCOL_NOT_FOUND,
-        PROTOCOL_ERROR_IP_SOURCE_UNKNOWN,
-        PROTOCOL_ERROR_IP_DEST_UNKNOWN,
+        PROTOCOL_ERROR_IP_SRCDEST_UNKNOWN,
         PROTOCOL_ERROR_IP_PROTOCOL_ATTR_NOT_UINT,
         PROTOCOL_ERROR_IP_PROTOCOL_ATTR_OUT_OF_RANGE,
         PROTOCOL_ERROR_PORT_PORTNUM_ATTR_NOT_FOUND,
@@ -274,8 +270,8 @@ public:
             }
         }
 
-    //maybe this should be a static member of ProtocolEntry?
-    ProtocolEntry::Score getScore(string const & s)
+    //maybe these should be a static member of ProtocolEntry?
+    ProtocolEntry::Score getScore( string const & s )
     {
         if( s == "low" )
             return ProtocolEntry::LOW;
@@ -286,14 +282,43 @@ public:
         else
             return ProtocolEntry::UNKNOWN;
     }
-//start element case functions
-    void caseProtocolStateOutside( QString const & localName )
+    ProtocolEntry::RangeType getRangeType(string const & s)
+    {
+        if( s == "any" )
+            return ProtocolEntry::ANY;
+        else if( s == "privileged" )
+            return ProtocolEntry::PRIVILEGED;
+        else if( s == "nonprivileged" )
+            return ProtocolEntry::NONPRIVILIGED;
+        else if( s == "dynamic" )
+            return ProtocolEntry::DYNAMIC;
+        else
+            return ProtocolEntry::RANGE;
+    }
+    
+    uint8_t getType ( string const & s )
+    {
+        if(s == "tcp")
+            return IPPROTO_TCP;
+        else if(s == "udp")
+            return IPPROTO_UDP;
+        else if(s == "icmp")
+            return IPPROTO_ICMP;
+        else if(s == "icmpv6")
+            return IPPROTO_ICMPV6;
+        else
+            return IPPROTO_IP;
+    }
+
+//start element Helper case functions
+    bool caseProtocolStateOutside( QString const & localName )
     {
         if( localName == "protocoldb" )
             return parsestate = PROTOCOL_STATE_PROTOCOLDB;
-        return unknowndepth++;
+        return ++unknowndepth;
     }
-    void caseProtocolStateProtocolDB( QString const & localName , QXmlAttributes const & atts )
+
+    bool caseProtocolStateProtocolDB( QString const & localName, QXmlAttributes const & atts )
     {
         int i;
         if( localName == "protocol" )
@@ -310,9 +335,10 @@ public:
             parsestate = PROTOCOL_STATE_ENTRY;
             return true;
         }
-        return unknowndepth++;
+        return ++unknowndepth;
     }
-    void caseProtocolStateEntry( QString const & localName, QXmlAttributes const & atts )
+
+    bool caseProtocolStateEntry( QString const & localName, QXmlAttributes const & atts )
     {
         if( localName == "longname" )
             return caseProtocolStateEntryLongName( atts );
@@ -327,64 +353,335 @@ public:
         else if( localName == "pragma" )
             return caseProtocolStateEntryPragma( atts );
         else
-            return unknowndepth++;
+            return ++unknowndepth;
     }
-        void caseProtocolStateEntryLongName(QXmlAttributes const & atts)
+
+    bool caseProtocolStateEntryLongName( QXmlAttributes const & atts )
+    {
+        string tmp = "en";
+        loadlongname = currententry.longnamelanguage.empty()
+        int i = atts.index( protocolnamespace, "lang" );
+        if( i != -1 )
+            tmp = atts.value(i).toStdString();
+        if( loadlongname )
+            currententry.longnamelanguage = tmp;
+        return parsestate = PROTOCOL_STATE_LONGNAME;
+    }
+
+    bool caseProtocolStateEntryDescription( QXmlAttributes const & atts )
+    {
+        string tmp = "en";
+        loaddescription = currententry.descriptionlanguage.empty();
+        int i = atts.index(protocolnamespace, "lang");
+        if( i != -1 )
+            tmp = atts.value(i).toStdString();
+        if( loaddescription )
+            currententry.descriptionlanguage = tmp;
+        return parsestate = PROTOCOL_STATE_DESCRIPTION;
+    }
+
+    bool caseProtocolStateEntryClassification( QXmlAttributes const & atts )
+    {
+        int i = atts.index(protocolnamespace, "class");
+        if ( i != -1 )
+            currententry.classification = atts.value(i).toStdString();
+        return parsestate = PROTOCOL_STATE_CLASSIFICATION;
+    }
+
+    bool caseProtocolStateEntryNetwork()
+    {
+        return parsestate = PROTOCOL_STATE_NETWORK;
+    }
+
+    bool caseProtocolStateEntrySecurity( QXmlAttributes const & atts )
+    {
+        int i = atts.index( protocolnamespace, "threat");
+        if( i != -1 )
+            currententry.threat = getScore( atts.value(i).toStdString() );
+        i = atts.index( protocolnamespace, "falsepos");
+        if( i != -1 )
+            currententry.fasepos = getScore( atts.value(i).toStdString() );
+        return parsestate = PROTOCOL_STATE_SECURITY;
+    }
+
+    bool caseProtocolStateEntryPragma( QXmlAttributes const & atts )
+    {
+        int i = atts.index( protocolnamespace, "name" );
+        if( i != -1 )
+            currententry.pragma[atts.value(i).toStdString()];
+        return parsestate = PROTOCOL_STATE_ENTRY_PRAGMA;
+    }
+
+    bool caseProtocolStateNetwork( QString const & localName, QXmlAttributes const & atts )
+    {
+        int i;
+        uint8_t type = getType( localName );
+        currentnetuse = ProtocolNetUse();   //i don't like this. i think i want to just have "current protocol", or get pdb.last()
+                                            //and then add a new netuse to that. and then modify it in place. like
+                                            //net * t = pdb.last().addNewNet();
+                                            //t->dostuff();
+        currentnetuse.setType( type );
+        //Handle Protocol Attribute
+        if(type == IPPROTO_IP)
         {
-            string tmp = "en";
-            loadlongname = currententry.longnamelanguage.empty()
-            int i = atts.index( protocolnamespace, "lang" );
+            i = atts.index( protocolnamespace, "protocol" );
+            if( i== -1)
+            {
+                errorstate = PROTOCOL_ERROR_IP_PROTOCOL_NOT_FOUND;
+                return false;
+            }
+            string temp = atts.value(i).toStdString();
+            try
+            { currentnetuse.setType( boost::lexical_cast<uint8_t>(temp) ); }
+            catch( ... )
+            {
+                errorstate = PROTOCOL_ERROR_IP_PROTOCOL_ATTR_NOT_UINT;//not that it should be.
+                return false;//this removes the need to range check.
+            }
+        }
+
+        //Handle Source Attribute
+        bool good = caseProtocolStateNetworkHandleAttribute( atts, type, &currentnetuse, &ProtocolNetUse::setSourceRole, "source" );
+        if(!good)
+            return false;
+        //Handle Dest Attribute
+        good = caseProtocolStateNetworkHandleAttribute( atts, type, &currentnetuse, &ProtocolNetUse::setDestRole, "dest" );
+        if(!good)
+            return false;
+        //Handle Direction Attribute
+        i = atts.index( protocolnamespace, "direction" );
+        if( i != -1 )
+            currentnetuse.setBidirectional(true);
+
+        switch(type)
+        {
+            case IPPROTO_TCP:  parsestate = PROTOCOL_STATE_TCP;  break;
+            case IPPROTO_UDP:  parsestate = PROTOCOL_STATE_UDP;  break;
+            case IPPROTO_ICMP: parsestate = PROTOCOL_STATE_ICMP; break;
+            case IPPROTO_IP:   parsestate = PROTOCOL_STATE_IP;   break;
+            default: return false;
+        }
+        return true;
+    }
+
+    bool caseProtocolStateNetworkHandleAttribute( QXmlAttributes const & atts, uint8_t type, ProtocolNetUse * cur, void (ProtocolNetUse::*f)(ProtocolNetUse::Role), string sym)
+    {
+        int i = atts.index( protocolnamespace, sym.c_str() );
+        if( i != -1 )
+        {
+            string t = atts.value(i).toStdString();
+            if( t == "client" )
+                cur->*f( ProtocolNetUse::CLIENT );
+            else if( t == "server" )
+                cur->*f( ProtocolNetUse::SERVER );
+            else
+            {
+                switch(type)
+                {
+                    case IPPROTO_TCP:  errorstate = PROTOCOL_ERROR_TCP_SRCDEST_UNKNOWN;
+                        break;
+                    case IPPROTO_UDP:  errorstate = PROTOCOL_ERROR_UDP_SRCDEST_UNKNOWN;
+                        break;
+                    case IPPROTO_ICMP: errorstate = PROTOCOL_ERROR_ICMP_SRCDEST_UNKNOWN;
+                        break;
+                    case IPPROTO_IP:   errorstate = PROTOCOL_ERROR_IP_SRCDEST_UNKNOWN;
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool caseProtocolStateTCP( QString const & localName, QXmlAttributes const & atts )
+    {
+        if( localName == "source" )
+            return parsestate = PROTOCOL_STATE_TCP_SOURCE;
+        if( localName == "dest" )
+            return parsestate = PROTOCOL_STATE_TCP_DEST;
+        if( localName == "description" )
+        {
+            caseProtocolStateDescriptionLanguage( atts );
+            return parsestate = PROTOCOL_STATE_TCP_DESCRIPTION;
+        }
+        if( localName == "pragma" )
+        {
+            int i = atts.index( protocolnamespace, "name" );
             if( i != -1 )
-                tmp = atts.value(i).toStdString();
-            if( loadlongname )
-                currententry.longnamelanguage = tmp;
-            return parsestate = PROTOCOL_STATE_LONGNAME;
+                currentnetuse.pragma[atts.value(i).toStdString()];
+            return parsestate = PROTOCOL_STATE_TCP_PRAGMA;
         }
-        void caseProtocolStateEntryDescription(QXmlAttributes const & atts);
+        return ++unknowndepth;
+    }
+
+    bool caseProtocolStateUDP( QString const & localName, QXmlAttributes const & atts )
+    {
+        if( localName == "source" )
+            return parsestate = PROTOCOL_STATE_UDP_SOURCE;
+        if( localName == "dest" )
+            return parsestate = PROTOCOL_STATE_UDP_DEST;
+        if( localName == "description" )
         {
-            string tmp = "en";
-            loaddescription = currententry.descriptionlanguage.empty();
-            int i = atts.index(protocolnamespace, "lang");
+            caseProtocolStateDescriptionLanguage( atts );
+            return parsestate = PROTOCOL_STATE_UDP_DESCRIPTION;
+        }
+        if( localName == "pragma" )
+        {
+            int i = atts.index( protocolnamespace, "name" );
             if( i != -1 )
-                tmp = atts.value(i).toStdString();
-            if( loaddescription )
-                currententry.descriptionlanguage = tmp;
-            return parsestate = PROTOCOL_STATE_DESCRIPTION;
+                currentnetuse.pragma[atts.value(i).toStdString()];
+            return parsestate = PROTOCOL_STATE_UDP_PRAGMA;
         }
-        void caseProtocolStateEntryClassification(QXmlAttributes const & atts)
+        return ++unknowndepth;
+        
+    }
+
+    bool caseProtocolStateICMP( QString const & localName, QXmlAttributes const & atts )
+    {
+        if( localName == "type" )
         {
-            int i = atts.index(protocolnamespace, "class");
-            if ( i != -1 )
-                currententry.classification = atts.value(i).toStdString();
-            return parsestate = PROTOCOL_STATE_CLASSIFICATION;
-        }
-        void caseProtocolStateEntryNetwork()
-        {
-            return parsestate = PROTOCOL_STATE_NETWORK;
-        }
-        void caseProtocolStateEntrySecurity(QXmlAttributes const & atts)
-        {
-            int i = atts.index( protocolnamespace, "threat");
+            currentnetuse.SourceRangeCode(-1);
+                        //we either don't specify the code, or there isn't one associated with the type
+            int i = atts.index( protocolnamespace, "value" )
+            if( i == -1 )
+            {
+                errorstate = PROTOCOL_ERROR_TYPE_VALUE_ATTR_NOT_FOUND;
+                return false;
+            }
+            currentnetuse.SourceRangeType( boost::lexical_cast<uint8_t>( atts.value(i).toStdString() ) );
+
+            i = atts.index( protocolnamespace, "code" );
             if( i != -1 )
-                currententry.threat = getScore( atts.value(i).toStdString() );
-            i = atts.index( protocolnamespace, "falsepos");
-            if( i != -1 )
-                currententry.fasepos = getScore( atts.value(i).toStdString() );
-            return parsestate = PROTOCOL_STATE_SECURITY;
+                currentnetuse.SourceRangeCode( boost::lexical_cast<uint8_t>( atts.value(i).toStdString() ) );
+            return parsestate = PROTOCOL_STATE_ICMP_TYPE;
         }
-        void caseProtocolStateEntryPragma(QXmlAttributes const & atts);
-    void caseProtocolStateNetwork();
-        void caseProtocolStateNetworkTCP();
-        void caseProtocolStateNetworkUDP();
-        void caseProtocolStateNetworkICMP();
-        void caseProtocolStateNetworkIP();
-    void caseProtocolStateTCP();
-    void caseProtocolStateUDP();
-    void caseProtocolStateICMP();
-    void caseProtocolStateIP();
-    void caseProtocolStateSrcDest()
-        void caseProtocolStateSrcDestPort();
-        void caseProtocolStateSrcDestRange();
+        if( localName == "description" )
+        {
+            caseProtocolStateDescriptionLanguage( atts );
+            return parsestate = PROTOCOL_STATE_UDP_DESCRIPTION;
+        }
+        if( localName == "pragma" )
+        {
+            int i = atts.index( protocolnamespace, "name" );
+            if( i != -1 )
+                currentnetuse.pragma[atts.value(i).toStdString()];
+            return parsestate = PROTOCOL_STATE_UDP_PRAGMA;
+        }
+        return ++unknowndepth;
+    }
+
+    bool caseProtocolStateIP( QString const & localName, QXmlAttributes const & atts )
+    {
+        if( localName == "description" )
+        {
+            caseProtocolStateDescriptionLanguage( atts );
+            return parsestate = PROTOCOL_STATE_UDP_DESCRIPTION;
+        }
+        if( localName == "pragma" )
+        {
+            int i = atts.index( protocolnamespace, "name" );
+            if( i != -1 )
+                currentnetuse.pragma[atts.value(i).toStdString()];
+            return parsestate = PROTOCOL_STATE_UDP_PRAGMA;
+        }
+        return ++unknowndepth;
+    }
+
+    bool caseProtocolStateNetDescriptionLanguage(QXmlAttributes & atts)
+    {
+        string tmp = "en";
+        int i = atts.index( protocolnamespace, "lang" )
+        if( i != -1 )
+            tmp = atts.value(i).toStdString();
+        loaddescription = currentnetuse.descriptionlanguage.empty();
+        if( loaddescription == true )
+            currentnetuse.descriptionlanguage = tmp;
+    }
+
+    bool caseProtocolStateSrcDest( QString const & localName, QXmlAttributes const & atts )
+    {
+        if( localName == "port" )
+            return caseProtocolStateSrcDestPort(atts);
+        if( localName == "range" )
+            return caseProtocolStateSrcDestPort(atts);
+        return ++unknowndepth;
+    }
+
+    /*!
+     *  starting to think that the xml parser should have "currentnetusedetail" or some future variant of it,
+     *  as well as currentnetuse. this gets rid of my problem where it was only used for parsing but always in memory
+     *  because this class only exists durring construction of the firewall manager.
+     *  but it still is annoying to me because then i need to have the functionality to modularly build a  detail, whereas right now
+     *  i like the idea of no ablility to look at or touch them outside of access member functions of the protocolEntry.
+     *  i guess i could have the class friend the xmlparser... :/
+     *  it could just be a pointer to the last defined PortRange. that is essentially what it was anyway.
+     */
+
+    void caseProtocolStateSrcDestPort( QXmlAttributes const & atts )
+    {//the main problem is right here i don't care if it is a src or dst, it is the same either way
+    currentnetusedetail = ProtocolNetUseDetail();
+        int i = atts.index( protocolnamespace, "portnum" );
+        if( i == -1 )
+        {
+            errorstate = PROTOCOL_ERROR_PORT_PORTNUM_ATTR_NOT_FOUND;
+            return false;
+        }
+        string s = atts.value(i).toStdString();
+        ProtocolEntry::RangeType type( s );
+        currentnetusedetail.setRangeType(type);
+        if(type == ProtocolEntry::RANGE)//the other range types' ports are decided elsewhere
+            currentnetusedetail.setPort( boost::lexical_cast<uint16_t>(s) );
+        switch(parsestate)
+        {//this makes me want to do some verilog like cleverness with the state values.
+            case PROTOCOL_STATE_TCP_SOURCE:
+                return parsestate = PROTOCOL_STATE_TCP_SOURCE_PORT;
+            case PROTOCOL_STATE_UDP_SOURCE:
+                return parsestate = PROTOCOL_STATE_UDP_SOURCE_PORT;
+            case PROTOCOL_STATE_TCP_DEST:
+                return parsestate = PROTOCOL_STATE_TCP_DEST_PORT;
+            case PROTOCOL_STATE_UDP_DEST:
+                return parsestate = PROTOCOL_STATE_TCP_DEST_PORT;
+            default:
+                return true;
+        }
+    }
+
+    void caseProtocolStateSrcDestRange()
+    {
+    currentnetusedetail = ProtocolNetUseDetail();
+
+        int i = atts.index( protocolnamespace, "start" );
+        if( i == -1 )
+        {
+            errorstate = PROTOCOL_ERROR_PORTRANGE_START_ATTR_NOT_FOUND;
+            return false;
+        }
+        currentnetusedetail.setStartPort( boost::lexical_cast<uint16_t>( atts.value(i).toStdString() ) );
+
+        i = atts.index( protocolnamespace, "end" );
+        if( i == -1 )
+        {
+            errorstate = PROTOCOL_ERROR_PORTRANGE_START_ATTR_NOT_FOUND;
+            return false;
+        }
+        currentnetusedetail.setEndPort( boost::lexical_cast<uint16_t>( atts.value(i).toStdString() ) );
+
+        switch(parsestate)
+        {
+            case PROTOCOL_STATE_TCP_SOURCE:
+                return parsestate = PROTOCOL_STATE_TCP_SOURCE_PORTRANGE;
+            case PROTOCOL_STATE_UDP_SOURCE:
+                return parsestate = PROTOCOL_STATE_UDP_SOURCE_PORTRANGE;
+            case PROTOCOL_STATE_TCP_DEST:
+                return parsestate = PROTOCOL_STATE_TCP_DEST_PORTRANGE;
+            case PROTOCOL_STATE_UDP_DEST:
+                return parsestate = PROTOCOL_STATE_TCP_DEST_PORTRANGE;
+            default:
+                return true;
+        }
+    }
 
 };
 
--- src/protocoldb.h
+++ src/protocoldb.h
@@ -24,6 +24,29 @@ email                : simon@simonzone.com
 //#include <iostream>
 #include <boost/foreach.hpp>
 
+class ProtocolEntry::PortRange
+
+
+class ProtocolEntry::ProtocolNetUse
+{
+    Role sourceRole;
+    PortRange sourceRange;
+    Role destRole;
+    PortRange destRange;
+    uint8_t protocolType
+public:
+//!the Roles a destination or source can play
+/*!
+ *In guarddog this was refered to as an Entity, i feel that
+ *Role is a bit better but maybe not.
+ */
+enum Role { SERVER, CLIENT };
+
+    void setSourceRole( Role );
+    void setDestRole( Role );
+
+};
+
 
 //! Holds all information relavant to a single Protocol
 /*!
@@ -36,12 +59,18 @@ class ProtocolEntry
 //just to make things tidier
 using std::string;
 
+    class ProtocolNetUse;
+    class PortRange;
+
     string name;//!<Holds the name of the Protocol
     string longName;//!<Holds the long name. This can be viewed as a short description. For UDPs the name and long name are the same.
     string longNameLanguage;//!<The language that the long name is in.
     string description;//!<Holds the description of the protocol. What programs use it, and what not.
     string descriptionLanguage;//!<The language the description is in. I don't know why this and the longnamelanguage would be different.
     string classification;//!<To what type the protocol belongs. It may be benifitial to make this a list of strings, so there can be more than one.
+
+    std::vector<ProtocolNet> source;
+    std::vector<ProtocolNet> dest;
 
     Score threat;//<!The threat level of a protocol, LOW, MEDIUM, or HIGH.
     Score falsepos;//<!no idea. Figure this one out.
@@ -70,12 +99,15 @@ using std::string;
 
 public:
 
-    //!Adds an Entry by data. The following is a good starting point for an Entry. Can be made with only a name, or things can be specified more directly.
-    //!It will probably always be usefull to change the entry after, especially when importing the XML file
+    //!Adds an Entry by name.
+    //!The networks of the Protocol are constructed empty
     //!Throws "ProtocolEntry *name* Already exists!"
-    void addProtocolEntry(string name, ProtocolEntry::RangeType rangeType=ProtocolEntry::RANGE, in_port_t startport = 0,
+    void addProtocolEntry(string name);
+    
+    //!Adds an Entry by data.
+    //!Adds a default network source and destination (the source being completly open, the destination being specified)
+    void addProtocolEntry(string name , ProtocolEntry::RangeType rangeType, in_port_t startport = 0,
                              in_port_t endport = 0, uint8_t portType = IPPROTO_TCP, bool bi = true);//in_port_t is an alias for uint16_t and is the "prefered" type for portnumbers
-
     //!Deletes an entry from the database by name.
     //!If (there shouldn't be) there is more than one entry with the same name, it deletes the first.
     void deleteProtocolEntry( string name );
@@ -93,3 +125,23 @@ public:
     //!Finds and returns a const reference to a Protocol Entry by name
     ProtocolEntry const & lookup( string const & name ) const;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
